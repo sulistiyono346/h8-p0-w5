bc795924b29f87ed8c0d86446805b7f19962cef0
Sulistiyono <sulistiyono@Sulistio-MacBook-Pro.local>
2018-07-24 16:02:30 +0700

    Tugas Hari Kamis

---
+++ exercises8.js
@@ -0,0 +1,10 @@
+function cariPelaku(str) {
+  var strAbc = (str.match(/abc/g));
+  return strAbc.length
+}
+
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
